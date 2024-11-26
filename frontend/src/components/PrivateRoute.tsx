0c6bd95b14bb125fb25df115cbadaf9cd84e18af
mominkas <mominkashif10@gmail.com>
2024-11-25 16:22:13 -0800

    cleanup

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import LoginService from "../services/LoginService";
 
@@ -8,14 +8,8 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const loginService = LoginService.getInstance();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    setIsAuthenticated(loginService.isLoggedIn());
-  }, [loginService]);
-  console.log("Check this!! " + isAuthenticated);
   if (!loginService.isLoggedIn()) {
-    console.log("Not authenticated, redirecting to login");
     return <Navigate to="/login" replace />;
   }
 
